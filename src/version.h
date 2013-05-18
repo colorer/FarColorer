285a0920af96fa58cd1875d2a63bd7e2597af38c
Aleksey Dobrunov <ctapmex@gmail.com>
2013-05-18 19:02:46 +0600

    fix build resource

--- src/version.h
+++ src/version.h
@@ -3,3 +3,9 @@
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
+#ifdef WIN64
+  #define CONF " (x64)"
+#else
+  #define CONF ""
+#endif
+#define FILE_DESCRIPTION "FarColorer - Syntax Highlighting for Far Manager 3.0" CONF
