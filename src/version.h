16dcae15a32bf4e3a369ee099212b1dd12aef154
Aleksey Dobrunov <ctapmex@gmail.com>
2013-06-14 14:25:39 +0600

    version info: update

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VER_FILEVERSION             1,1,2,1
-#define VER_FILEVERSION_STR         "1.1.2.1"
+#define VER_FILEVERSION             1,2,1,0
+#define VER_FILEVERSION_STR         "1.2.1.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
