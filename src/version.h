c64092e638f279f27599ec9bcdba41527b5915c0
Aleksey Dobrunov <ctapmex@gmail.com>
2020-04-21 22:50:42 +0500

    version info

--- src/version.h
+++ src/version.h
@@ -1,11 +1,12 @@
 #define VER_FILEVERSION 1, 3, 6, 0
 #define VER_FILEVERSION_STR "1.3.6.0"
 
-#define VER_PRODUCTVERSION VER_FILEVERSION
-#define VER_PRODUCTVERSION_STR VER_FILEVERSION_STR
-#ifdef WIN64
+#ifdef _WIN64
 #define CONF " (x64)"
 #else
-#define CONF ""
+#define CONF " (x86)"
 #endif
 #define FILE_DESCRIPTION "FarColorer - Syntax Highlighting for Far Manager 3.0" CONF
+
+#define VER_PRODUCTVERSION VER_FILEVERSION
+#define VER_PRODUCTVERSION_STR VER_FILEVERSION_STR CONF
