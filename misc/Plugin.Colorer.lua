1de349e4b0b13cd0f9b6b2fa43b665b31d87d999
Aleksey Dobrunov <ctapmex@gmail.com>
2021-05-05 10:16:04 +0500

    update lua script

--- misc/Plugin.Colorer.lua
+++ misc/Plugin.Colorer.lua
@@ -1,44 +1,5 @@
 local ColorerGUID = "D2F36B62-A470-418D-83A3-ED7A3710E5B5";
 
-local function ColorerProcess()
-  local KeysArray = {
-    ["Alt'"] = "6",    -- find errors
-    ["RAlt'"] = "6",    -- find errors
-    ["Alt;"] = "5",    -- list functions
-    ["RAlt;"] = "5",    -- list functions
-    AltK     = "7",    -- select region
-    RAltK     = "7",    -- select region
-    F5       = "5",    -- list functions
-    AltL     = "1",    -- list types
-    RAltL     = "1",    -- list types
-    AltO     = "8",    -- find function
-    RAltO     = "8",    -- find function
-    AltP     = "4",    -- select pair
-    RAltP     = "4",    -- select pair
-    AltR     = "R",    -- reload colorer
-    RAltR     = "R",    -- reload colorer
-    ["Alt["] = "2",    -- match pair
-    ["RAlt["] = "2",    -- match pair
-    ["Alt]"] = "3",    -- select block
-    ["RAlt]"] = "3",    -- select block
-    CtrlShiftC = "C Tab",     -- cross on/off
-    RCtrlShiftC = "C Tab"     -- cross on/off
-  };
-  local akey = mf.akey(2);
-  local key = KeysArray[mf.akey(2)];
-  Plugin.Menu(ColorerGUID) ;
-  if Object.CheckHotkey(key) ~= 0 then
-    Keys(key);
-    if  akey == "CtrlShiftC" or akey == "RCtrlShiftC" then
-      State=Dlg.GetValue(3, 0);
-      if State==0 then Keys("Add") else Keys("Subtract") end
-      Keys("Enter")
-    end
-  else
-    Keys("Esc")
-  end
-end
-
 Macro {
   area="Editor";
   description="Colorer: Find Errors";
@@ -46,7 +7,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Errors", "Show")
+  end;
 }
 
 Macro {
@@ -56,7 +19,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Functions", "Show")
+  end;
 }
 
 Macro {
@@ -66,7 +31,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Functions", "Show")
+  end;
 }
 
 Macro {
@@ -76,7 +43,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Region", "Select")
+  end;
 }
 
 Macro {
@@ -86,7 +55,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Types", "Menu")
+  end;
 }
 
 Macro {
@@ -96,7 +67,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Functions", "Find")
+  end;
 }
 
 Macro {
@@ -106,7 +79,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Brackets", "SelectAll")
+  end;
 }
 
 Macro {
@@ -116,7 +91,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Settings", "Reload")
+  end;
 }
 
 Macro {
@@ -126,7 +103,9 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
+  action=function()
+    Plugin.Call(ColorerGUID, "Brackets", "SelectAll")
+  end;
 }
 
 Macro {
@@ -136,15 +115,7 @@ Macro {
   condition = function()
     return Plugin.Exist(ColorerGUID)
   end;
-  action=ColorerProcess;
-}
-
-Macro {
-  area="Editor";
-  description="Colorer: Cross on/off";
-  key="/.CtrlShiftC/";
-  condition = function()
-    return Plugin.Exist(ColorerGUID)
+  action=function()
+    Plugin.Call(ColorerGUID, "Brackets", "SelectIn")
   end;
-  action=ColorerProcess;
 }
