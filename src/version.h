e15d7cff79602ad40dfcf95f5a63206610ddc6c2
Aleksey Dobrunov <ctapmex@gmail.com>
2021-06-14 22:59:46 +0500

    update version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VER_FILEVERSION 1, 3, 27, 0
-#define VER_FILEVERSION_STR "1.3.27.0"
+#define VER_FILEVERSION 1, 3, 28, 0
+#define VER_FILEVERSION_STR "1.3.28.0"
 
 #ifdef _WIN64
 #define CONF " (x64)"
