99e67695f6d7ff0d4b352a0ea62005b67eef552d
Aleksey Dobrunov <ctapmex@gmail.com>
2021-06-14 22:59:04 +0500

    Enable attributes in classic mode, add strikeout

--- src/FarEditor.cpp
+++ src/FarEditor.cpp
@@ -1220,16 +1220,18 @@ FarColor FarEditor::convert(const StyledRegion* rd) const
     if (rd->bback) {
       col.BackgroundColor = rd->back;
     }
-    if (TrueMod) {
-      if (rd->style & StyledRegion::RD_BOLD) {
-        col.Flags |= FCF_FG_BOLD;
-      }
-      if (rd->style & StyledRegion::RD_ITALIC) {
-        col.Flags |= FCF_FG_ITALIC;
-      }
-      if (rd->style & StyledRegion::RD_UNDERLINE) {
-        col.Flags |= FCF_FG_UNDERLINE;
-      }
+
+    if (rd->style & StyledRegion::RD_BOLD) {
+      col.Flags |= FCF_FG_BOLD;
+    }
+    if (rd->style & StyledRegion::RD_ITALIC) {
+      col.Flags |= FCF_FG_ITALIC;
+    }
+    if (rd->style & StyledRegion::RD_UNDERLINE) {
+      col.Flags |= FCF_FG_UNDERLINE;
+    }
+    if (rd->style & StyledRegion::RD_STRIKEOUT) {
+      col.Flags |= FCF_FG_UNDERLINE;
     }
   }
 
